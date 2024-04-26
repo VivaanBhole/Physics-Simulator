ef27fdb626643f0113e8fd4de7b3c2d985741832
VivaanBhole <bhole.vivaan8421@gmail.com>
2024-04-26 13:05:24 -0400

    changed Simulator scene name, added sample block prefab

--- Assets/Scripts/Engine/MainMenu/MainButton.cs
+++ Assets/Scripts/Engine/MainMenu/MainButton.cs
@@ -13,6 +13,6 @@ public class MainButton : MonoBehaviour, IPointerClickHandler
     }
     public void OnPointerClick(PointerEventData data)
     {
-        
+        SceneManager.LoadScene("Simulator");
     }
 }
