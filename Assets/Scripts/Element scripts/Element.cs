5428d6505ca268bf06720e7687ddf3a8d547fa7b
VivaanBhole <bhole.vivaan8421@gmail.com>
2024-05-28 23:38:22 -0400

    added magnet

--- Assets/Scripts/Element scripts/Element.cs
+++ Assets/Scripts/Element scripts/Element.cs
@@ -8,7 +8,8 @@ public class Element : MonoBehaviour
     //Mass, charge, isConuctive, isLocked, velocity, acceleration, forces
 
     public float Mass, Charge;
-    public Vector3 Velocity, Acceleration, MagneticForce, ElectricForce, GravitationalForce, NormalForce;
+    public Vector3 Velocity, Acceleration, MagneticForce, ElectricForce;
+    public ElementData Data;
 
     public virtual void SetMass(float m) { Mass = m; }
     public virtual void SetCharge(float c) { Charge = c; }
@@ -17,10 +18,7 @@ public class Element : MonoBehaviour
     public virtual void SetWidth(float w) { }
     public virtual void SetHollow(float h) { }
     public virtual void SetConductivity(float c) { }
-    public virtual void SetAreaChargeDensity(float d) { }
-    public virtual void SetVolumeChargeDensity(float d) { }
-    public virtual void SetLinearChargeDensity(float d) { }
-    public virtual void SetFinite(float f) { }
+    public virtual void SetPoleStrength(float d) { }
     public virtual Vector3 GetMagneticForce(Element other) { return Vector3.zero; }
     public virtual Vector3 GetElectricForce(Element other) { return Vector3.zero; }
 
